7550990cf954137526396c27b31d437b518bb178
Mohammed Madni <mohammedmadni09@gmail.com>
2025-02-18 14:22:15 +0530

    chore: add cors as a dependency

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,9 @@
 import express from 'express';
-import userRouter from './routes/userRoute.js';
+import userRouter from './routes/userRoute';
 import bodyParser from 'body-parser';
-import reviewRouter from './routes/reviewRoutes.js';
-import booksRouter from './routes/booksRoutes.js';
+import cors from 'cors';
+import reviewRouter from './routes/reviewRoutes';
+import booksRouter from './routes/booksRoutes';
 
 const app = express();
 const port = 3000;
@@ -11,6 +12,7 @@ app.use(bodyParser.json({ limit: "35mb" }));
 app.use('/api', userRouter);
 app.use('/api',reviewRouter);
 app.use('/api',booksRouter);
+app.use(cors({ credentials: true, origin: true }));
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
