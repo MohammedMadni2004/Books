eba64257ba33bd9de84fcd1f6afa85cf33ece22b
Mohammed Madni <mohammedmadni09@gmail.com>
2025-02-18 12:52:28 +0530

    refactor: remove the test route for checking admin

--- backend/src/routes/userRoute.ts
+++ backend/src/routes/userRoute.ts
@@ -8,6 +8,5 @@ const userRouter: Router = express.Router();
 userRouter.post("/login", login);
 userRouter.post("/register", register);
 userRouter.get("/getUser/", isAuthenticated, getUser);
-userRouter.get('/test',isAuthenticated,checkAdmin) ; 
 
 export default userRouter;
